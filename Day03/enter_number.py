a58267582c7e0c77833cffb726670b91a29d3f34
LeeSoeun <soeun318@naver.com>
2025-03-06 10:10:27 +0900

    코드 수정 및 보완

--- Day03/enter_number.py
+++ Day03/enter_number.py
@@ -1,15 +1,15 @@
 # 여러 선택지 중 하나를 선택해서 입력받는 while문 만들기
 prompt = """
- 1. Add
- 2. Del
- 3. List
- 4. Quit
+1. Add
+2. Del
+3. List
+4. Quit
 
-Enter number: 
+Enter number:
 """
 
 number = 0
 
-while number != 4:
+while number != 4: 
   print(prompt)
-  number = int(input()) # 사용자 입력
+  number = int(input()) # 사용자 입력값 받기
